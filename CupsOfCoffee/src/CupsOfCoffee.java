19b382dfb3a9573d52dbd46d47ae91f990433104
jaycorri <mavericsumo@gmail.com>
2017-02-23 05:29:21 -0800

    wrote code from welcome message ending to payment method. Still troubleshooting correct use of switch.

---
+++ CupsOfCoffee/src/CupsOfCoffee.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+
+/**
+ * @author DouglasJ
+ *
+ */
+
+import java.util.Scanner; // necessary for Scanner class
+
+public class CupsOfCoffee {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Display a Welcome Message
+		System.out.println("Welcome to Jimmy's Coffee Shop");
+		
+		//assign variables
+		String firstname;
+		int cups;
+		double salesTax = 0.08;//needs to be constant/non-variable
+		double price = 1.95;//no instructions to set price!
+		Scanner keyboard = new Scanner(System.in);
+		
+		
+		//ask for the customer's first name
+		System.out.print("What is your first name? ");
+		String name = keyboard.nextLine();
+		
+		
+		//ask how many cups of coffee they will purchase
+		System.out.println("Hi " + name + ", how many cups fo coffee would you like?");
+		cups = keyboard.nextInt();
+		double Total = cups*price*salesTax;
+		
+		//have the user enter their payment type
+		System.out.println("That will be " + Total + ", and how will you be paying for that?\nType M if you are paying with cash.\nType C if you are paying by credit Card.\nType S if you will be using store credit.\nType G if you are paying with gold.");
+		String payOption = keyboard.next();
+		
+		//test program thus far
+		System.out.print(payOption);
+
+		
+		//four option switch or invalid and terminate program
+		/*broken code
+		switch(payOption)
+		{
+		case payOption == :
+			
+		break;
+		}
+		*/
+		//if-else-if statements with four outcomes then terminate program
+
+	}
+
+}
